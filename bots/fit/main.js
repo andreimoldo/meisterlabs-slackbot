5f8a66fe24c8344be9d79ac871cffd8036ff4a94
Andrei Moldovan <andrei@meisterlabs.com>
2015-06-20 12:45:30 +0200

    Added interval and new exercises.

--- bots/fit/main.js
+++ bots/fit/main.js
@@ -1,9 +1,10 @@
-var SlackBot = require('slackbots');
+var SlackBots = require('slackbots');
 var _ = require('lodash');
-var moment = require('moment')
+var moment = require('moment-timezone');
 
 function FitBot(params) {
     this._name = params.name;
+    this._emoji = ':muscle:';
     this._api = null;
 
     this.init();
@@ -12,17 +13,25 @@ function FitBot(params) {
 FitBot.prototype.EXERCISES = [
     {
         name: 'PUSHUPS',
-        max_reps: 10
+        quantity: '10'
     },
     {
         name: 'LUNGES',
-        max_reps: 15
+        quantity: '15'
+    },
+    {
+        name: 'WALL SIT',
+        quantity: '30 SECONDS'
+    },
+    {
+        name: 'PLANK',
+        quantity: '30 SECONDS'
     }
 ];
 
 FitBot.prototype.init = function() {
     console.log(this._name, 'initializing...');
-    this._api = new SlackBot({
+    this._api = new SlackBots({
         token: process.env.SLACK_TOKEN,
         name: this._name
     }).on('open', function() {
@@ -31,18 +40,22 @@ FitBot.prototype.init = function() {
 };
 
 FitBot.prototype._start = function() {
-    // TODO: check if it's the right time to send a message
-    // Monday - Friday
-    // 0900 - 17000
+    console.log(this._name, 'started');
+    this._sendChallenge();
+};
 
-    // get active users
-    this._getActiveUsers(function(activeUsers) {
-        // get victim
-        var victim = activeUsers[Math.floor(Math.random() * activeUsers.length)];
-        // get exercise
-        var exercise = this.EXERCISES[Math.floor(Math.random() * this.EXERCISES.length)];
-        console.log('@', victim.name, exercise.max_reps, exercise.name, 'NOW!');
-    }.bind(this));
+FitBot.prototype._sendChallenge = function() {
+    var now = moment.tz('Europe/Berlin');
+    if (now.day() != 0 && now.day() != 6 && now.hours() > 9 && now.hours() < 17) {
+        this._getActiveUsers(function(activeUsers) {
+            var victim = activeUsers[Math.floor(Math.random() * activeUsers.length)];
+            var exercise = this.EXERCISES[Math.floor(Math.random() * this.EXERCISES.length)];
+            console.log('@', victim.name, exercise.quantity, exercise.name, 'NOW!');
+        }.bind(this));
+    } else {
+        console.log(this._name, 'is not allowed to send challenges yet!');
+    }
+    setTimeout(this._sendChallenge.bind(this), 10000);
 };
 
 FitBot.prototype._getActiveUsers = function(callback) {
